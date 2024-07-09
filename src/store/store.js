7ee42826c4b471e008ed5b41acbcc9b1e8b75fca
seanErmilov <seanermilov321@gmail.com>
2024-07-09 15:13:58 +0300

    init repo

---
+++ src/store/store.js
@@ -0,0 +1,16 @@
+import { combineReducers, compose, legacy_createStore as createStore } from "redux"
+
+import { userService } from "../services/user.service.js"
+import { carReducer } from "./reducers/car.reducer.js"
+import { userReducer } from "./reducers/user.reducer.js"
+
+
+const rootReducer = combineReducers({
+    carModule: carReducer,
+    userModule: userReducer,
+})
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+export const store = createStore(rootReducer, composeEnhancers())
+
+window.gStore = store
